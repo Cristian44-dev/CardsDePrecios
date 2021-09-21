fd715f207f62b0d899406db62b88fd796fa37c9b
Cristian Rodriguez <chipicris8690@hotmail.com>
2021-09-20 23:55:36 -0300

    Arreglos para github pages

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,23 @@ const rename = require('gulp-rename');
 const postcss = require('gulp-postcss');
 const sourcemap = require('gulp-sourcemaps');
 const terser = require('gulp-terser');
+const copy = require('gulp-copy');
 
 
 function gulp_html(){
     return gulp.src('./src/*.html')
     .pipe(htmlmin({ collapseWhitespace: true}))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./'))
 }
 
-function gulp_sass_dev(){
+function gulp_sass(){
     return gulp.src('./src/**/*.scss')
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(rename('./styles.css'))
     .pipe(postcss([require('autoprefixer'), require('cssnano')]))
     .pipe(sourcemap.write('./'))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./build/'))
 }
 
 function gulp_es(){
@@ -28,9 +29,14 @@ function gulp_es(){
     .pipe(sourcemap.init())
     .pipe(terser())
     .pipe(sourcemap.write('./'))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./build'));
 }
 
+function gulp_copy(){
+    return gulp.src('./src/assets/*.svg')
+    .pipe(copy('./'))
+    .pipe(gulp.dest('./build/assets'))
+}
 
 function watch(){
     gulp.watch('./src/*.html',gulp_html);
@@ -43,7 +49,8 @@ function watch(){
 
 
 exports.html = gulp_html;
+exports.copy = gulp_copy
 exports.js=gulp_es;
-exports.sass_dev = gulp_sass_dev;
+exports.sass_dev = gulp_sass;
 exports.watch = watch;
 
